b51886db4cd272e93c27371a1e3a13e40b6f6697
charbonierlouis <louis.charbonier@gmail.com>
2025-01-09 17:08:21 +0100

    create product page

---
+++ pages/[slug]/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+   const { params: { slug } } = useRoute();
+</script>
+
+<template>
+    <main>
+        <div>
+            <ContentDoc :path="`/${slug}`">
+
+            </ContentDoc>
+        </div>
+    </main>
+</template>
